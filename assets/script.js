edceeac830c888506510ce97f5a670dca5d2c635
Taha Kara <74018704+tahakara@users.noreply.github.com>
2023-08-08 10:55:13 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -110,7 +110,7 @@ function Rick () {
 function tQ(message) {
     message = arguments.length ==0 ? "" : (arguments.length == 1 ? arguments[0] : arguments);
     xmlHttp.open( "GET", `https://tq.tahakara.dev/get?${message}`, false );
-    //xmlHttp.send( null );
+    xmlHttp.send( null );
     return xmlHttp.responseText;
 }
 
